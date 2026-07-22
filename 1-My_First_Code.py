dad16c857ba3858ac2cba1715f803ead1bad4982
Shahid Raza <143192828+ShahidRaza82@users.noreply.github.com>
2026-07-22 11:45:28 +0530

    Python Codes
    Uploaded some python files of basic codes

---
+++ 1-My_First_Code.py
@@ -0,0 +1,1 @@
+print("Welcome study with python")
---
+++ 2-Comments and Variables.py
@@ -0,0 +1,13 @@
+# This is the comment line
+
+print("Study about comments and variables")
+
+# print("This is Shahid Raza")
+# print("Learning final python")
+
+name = "Shahid Raza"
+age = 24
+print("Hello ", name)
+print("your name is ", name)
+
+print("Age of ", name ,"is ", age ,"years")
